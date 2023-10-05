fde60fc324ee313c3ba7af73f64668d18cb22b20
michael-onsight <michaelbenjamin12.mb@gmail.com>
2023-10-05 09:16:16 +0200

    New colour scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./docs/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'primary-color': '#277BC0',     // Add your primary color
+        'secondary-color': '#FFB200',   // Add your secondary color
+        'tertiary-color': '#FFCB42',    // Add your tertiary color
+        'quaternary-color': '#FFF4CF',  // Add your quaternary color
+      },
+    },
   },
   plugins: [],
 }
