7b9695ef86915b876669ee321cb0c374ed630904
michael-onsight <michaelbenjamin12.mb@gmail.com>
2023-10-05 10:44:47 +0200

    Darker navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
+        'dark-color': '#142e41',        // Add your dark color
         'primary-color': '#26577C',     // Add your primary color
         'secondary-color': '#E55604',   // Add your secondary color
         'tertiary-color': '#EBE4D1',    // Add your tertiary color
